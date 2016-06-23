3f6328b5ac0c9d77d65a062ca870b4116b1174ec
Perttu Ahola <celeron55@gmail.com>
2016-06-24 00:21:31 +0300

    lcd.h,cpp

---
+++ src/lcd.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <Arduino.h>
+
+extern uint8_t g_display_data[];
+
+extern const uint8_t SEG_I_14[] PROGMEM;
+
+extern const uint16_t CHAR_MAP_14SEG[] PROGMEM;
+
+void set_segment_char(uint8_t seg_i, char c);
+void set_segments(uint8_t i0, const char *text);
+
