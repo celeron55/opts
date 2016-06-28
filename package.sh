161a43fc7bb5f53e90e815778aa944b7d6caaad6
Perttu Ahola <celeron55@gmail.com>
2016-06-28 13:54:05 +0300

    package.sh

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ver=$(date +%Y%m%d.%H%M%S)
+rm -rf "/tmp/opts-$ver"
+mkdir "/tmp/opts-$ver"
+mkdir "/tmp/opts-$ver/src"
+cp src/*.cpp "/tmp/opts-$ver/src"
+cp src/*.hpp "/tmp/opts-$ver/src"
+cp src/*.h "/tmp/opts-$ver/src"
+mkdir "/tmp/opts-$ver/common"
+cp common/*.hpp "/tmp/opts-$ver/common"
+cp build.sh "/tmp/opts-$ver/"
+cd /tmp
+tar cjf "opts-$ver.tar.gz" "opts-$ver"
