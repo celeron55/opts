812b3148e195ca97956ee87163f8417f3ef60e00
Perttu Ahola <celeron55@gmail.com>
2016-06-30 23:19:16 +0300

    write_usb_fw.sh: Don't unmount automatically

--- write_usb_fw.sh
+++ write_usb_fw.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-set -euv
+set -eu
 MOUNT=$1
-cp -r build.sh src common arduino misc $MOUNT/FW/ && rm -f $MOUNT/FW/upgraded.txt && umount $MOUNT/
+cp -r build.sh src common arduino misc $MOUNT/FW/ &&
+rm -f $MOUNT/FW/upgraded.txt &&
+echo "You can now run: umount \"$MOUNT\"" ||
+echo "Failed!"
