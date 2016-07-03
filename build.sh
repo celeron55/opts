4f7612efef6adba974daba4015d5d896a6f6efea
Perttu Ahola <celeron55@gmail.com>
2016-07-03 11:20:55 +0300

    Move "rm -f player; cp opts player" from build.sh to be added into FW/build.sh in write_usb_fw.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-g++ -o opts src/main.cpp src/c55_getopt.cpp src/file_watch.cpp src/filesys.cpp src/arduino_firmware.cpp src/mkdir_p.cpp `pkg-config --libs --cflags mpv` --std=c++0x -Wall -Wno-unused-function -g &&
-rm -f player && cp opts player
+g++ -o opts src/main.cpp src/c55_getopt.cpp src/file_watch.cpp src/filesys.cpp src/arduino_firmware.cpp src/mkdir_p.cpp `pkg-config --libs --cflags mpv` --std=c++0x -Wall -Wno-unused-function -g
--- write_usb_fw.sh
+++ write_usb_fw.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -eu
 MOUNT=$1
-cp -r build.sh src common arduino misc $MOUNT/FW/ &&
-rm -f $MOUNT/FW/upgraded.txt &&
+cp -r build.sh src common arduino misc "$MOUNT"/FW/ &&
+echo "rm -f player; cp opts player" >> "$MOUNT"/FW/build.sh
+rm -f "$MOUNT"/FW/upgraded.txt &&
 echo "You can now run: umount \"$MOUNT\"" ||
 echo "Failed!"
